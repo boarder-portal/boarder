a473e885b9d2a8124b6ff6f2423a86560525e70a
droooney <d.vor7@mail.ru>
2016-12-01 04:18:31 +0500

    Set up root state.

---
+++ app/client/blocks/Dropdown/index.js
@@ -0,0 +1,61 @@
+import { D, Block, body } from 'dwayne';
+import template from './index.pug';
+
+const dropdownSelector = '.dropdown';
+let activeDropdown;
+
+class Dropdown extends Block {
+  static template = template();
+
+  visible = false;
+  elem = null;
+
+  afterRender() {
+    const { elem } = this;
+
+    elem.$[0].boarderDropdown = this;
+
+    elem
+      .find('[action="close"]')
+      .on('click', this.close);
+  }
+
+  open() {
+    this.visible = true;
+
+    if (activeDropdown && activeDropdown !== this) {
+      activeDropdown.close();
+    }
+
+    activeDropdown = this;
+  }
+
+  close = () => {
+    this.visible = false;
+    activeDropdown = null;
+  };
+}
+
+body.on('click', ({ target }) => {
+  target = D(target);
+
+  if (!activeDropdown) {
+    return;
+  }
+
+  const dropdown = target.closest(dropdownSelector);
+
+  if (!dropdown.length) {
+    return activeDropdown.close();
+  }
+
+  const dropdownInstance = dropdown.$[0].boarderDropdown;
+
+  if (dropdownInstance === activeDropdown) {
+    return;
+  }
+
+  activeDropdown.close();
+});
+
+Block.Dropdown = Dropdown;
