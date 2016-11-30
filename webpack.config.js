bcac12fb93f845091edd2fffdda03c50dbc94bdb
droooney <d.vor7@mail.ru>
2016-12-01 04:18:31 +0500

    Set up root state.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const autoprefixer = require('autoprefixer');
 
 module.exports = {
   entry: './app/client/index.js',
@@ -11,18 +10,9 @@ module.exports = {
     loaders: [
       { test: /.js$/, loader: 'babel!eslint', exclude: /node_modules/ },
       { test: /.json$/, loader: 'json' },
-      { test: /.pug$/, loader: 'pug', exclude: [/node_modules/] },
-      { test: /\.css$/, loader: 'style!css!postcss' },
-      { test: /\.less$/, loader: 'style!css!postcss!less' },
-      { test: /\.scss$/, loader: 'style!css!postcss!sass' },
-      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+      { test: /.pug$/, loader: 'pug', exclude: [/node_modules/] }
     ]
   },
-  postcss: [
-    autoprefixer({
-      browsers: ['last 2 versions']
-    })
-  ],
   watch: true,
   devtool: 'cheap-module-eval-source-map'
 };
