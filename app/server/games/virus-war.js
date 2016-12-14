4395f32960d670bac865ce59302dabbdb7d99677
droooney <d.vor7@mail.ru>
2016-12-14 22:22:08 +0500

    Set uo virus war.

--- app/server/games/virus-war.js
+++ app/server/games/virus-war.js
@@ -1,7 +1,26 @@
 const D = require('dwayne');
 const Game = require('./');
 
-const { array } = D;
+const {
+  array,
+  switcher
+} = D;
+
+const width = 10;
+const height = 10;
+
+const virusCellSwitcher = switcher('call', {
+  player: null,
+  type: null
+})
+  .case(({ x, y }) => x === 0 && y === 0, (players) => ({
+    player: players.$[0].login,
+    type: 'virus'
+  }))
+  .case(({ x, y }) => x === width - 1 && y === height - 1, (players) => ({
+    player: players.$[1].login,
+    type: 'virus'
+  }));
 
 /**
  * @class VirusWarGame
@@ -12,11 +31,11 @@ class VirusWarGame extends Game {
   prepareGame() {
     super.prepareGame();
 
-    this.field = array(10, (y) => (
-      array(10, (x) => ({
+    this.field = array(height, (y) => (
+      array(width, (x) => ({
         x,
         y,
-        shape: null
+        ...virusCellSwitcher({ x, y }, [this.players])
       })).$
     )).$;
     this.setColors();
