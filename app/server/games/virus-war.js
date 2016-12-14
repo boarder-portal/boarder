e50176ef78e6ee62b92ffc7beacfc37a9231eb33
droooney <d.vor7@mail.ru>
2016-12-14 21:11:18 +0500

    Move Lobby, Room and Player classes to a single folder.

---
+++ app/server/games/virus-war.js
@@ -0,0 +1,14 @@
+const Game = require('./');
+
+/**
+ * @class VirusWarGame
+ * @extends Game
+ * @public
+ */
+class VirusWarGame extends Game {
+  prepareGame() {
+    super.prepareGame();
+  }
+}
+
+module.exports = VirusWarGame;
