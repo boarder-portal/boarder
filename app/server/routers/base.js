5251a14e67cde8982b028d7ddd82bbf1218f1437
atooxa <attooxa@yandex.ru>
2016-12-16 05:49:46 +0500

    Set up flexbox.

--- app/server/routers/base.js
+++ app/server/routers/base.js
@@ -3,10 +3,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { session } = require('../controllers/session');
-const { ASSETS_PATH } = require('../../config/constants.json');
+const { ASSETS_PATH, VENDOR_PATH } = require('../../config/constants.json');
 
 module.exports = (app) => {
   app.use(ASSETS_PATH, express.static(path.resolve('./public')));
+  app.use(VENDOR_PATH, express.static(path.resolve('./node_modules')));
 
   app.use(session);
   app.use(bodyParser.urlencoded({ extended: false }));
