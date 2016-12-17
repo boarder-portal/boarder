2c77436d1afdeedd957430d4d5208a8b1a1e8961
droooney <d.vor7@mail.ru>
2016-12-17 19:46:17 +0500

    Finish virus war basics.

--- app/server/games/virus_war.js
+++ app/server/games/virus_war.js
@@ -1,9 +1,21 @@
 const D = require('dwayne');
 const Game = require('./');
+const { getAvailableCells } = require('../../shared/virus-war');
 const {
   games: {
+    global: {
+      events: {
+        game: { END_TURN }
+      }
+    },
     virus_war: {
-      virusesTypes: { VIRUS },
+      events: {
+        game: { SET_CELL }
+      },
+      virusesTypes: {
+        VIRUS,
+        FORTRESS
+      },
       virusesShapes: shapesObject
     }
   }
@@ -24,12 +36,10 @@ const virusCellSwitcher = switcher('call', {
 })
   .case(({ x, y }) => x === 0 && y === 0, (players) => ({
     player: players.$[0].login,
-    shape: players.$[0].data.shape,
     type: VIRUS
   }))
   .case(({ x, y }) => x === width - 1 && y === height - 1, (players) => ({
     player: players.$[1].login,
-    shape: players.$[1].data.shape,
     type: VIRUS
   }));
 
@@ -39,9 +49,21 @@ const virusCellSwitcher = switcher('call', {
  * @public
  */
 class VirusWarGame extends Game {
+  static listeners = {
+    [SET_CELL]: {
+      forActivePlayer: true,
+      listener: 'onSetCell'
+    },
+    [END_TURN]: {
+      forActivePlayer: true,
+      listener: 'onEndTurn'
+    }
+  };
+
   prepareGame() {
     super.prepareGame();
 
+    this.movesCount = 0;
     this.field = array(height, (y) => (
       array(width, (x) => ({
         x,
@@ -49,6 +71,9 @@ class VirusWarGame extends Game {
         ...virusCellSwitcher({ x, y }, [this.players])
       })).$
     )).$;
+    this.players.forEach((player) => {
+      player.score = 1;
+    });
     this.players.$[0].active = true;
     this.setColors();
     this.setShapes();
@@ -66,6 +91,34 @@ class VirusWarGame extends Game {
     players.forEach((player, i) => {
       player.data.shape = newShapes[i];
     });
+  }
+
+  onSetCell({ x, y }, socket) {
+    const { player } = socket;
+    const availableCells = getAvailableCells(this.field, player);
+    const cell = this.field[y][x];
+
+    if (!availableCells.includes(cell)) {
+      return;
+    }
+
+    if (cell.player && cell.type === VIRUS) {
+      cell.type = FORTRESS;
+      this.players.find(({ login }) => cell.player === login).value.score--;
+      cell.player = player.login;
+      this.emit(SET_CELL, cell, true);
+    } else if (!cell.player && this.movesCount < 3) {
+      cell.type = VIRUS;
+      socket.player.score++;
+      this.movesCount++;
+      cell.player = player.login;
+      this.emit(SET_CELL, cell, true);
+    }
+  }
+
+  onEndTurn() {
+    this.movesCount = 0;
+    this.changeTurn(true);
   }
 }
 
