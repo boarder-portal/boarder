1deb8a256fb5d016f11ced227a887be666978ce6
droooney <d.vor7@mail.ru>
2016-12-17 19:46:17 +0500

    Finish virus war basics.

--- app/client/games/VirusWar/index.js
+++ app/client/games/VirusWar/index.js
@@ -1,6 +1,20 @@
 import { D, Block } from 'dwayne';
 import template from './index.pug';
 import { getAvailableCells } from '../../../shared/virus-war';
+import { games as gamesConfig } from '../../../config/constants.json';
+
+const {
+  global: {
+    events: {
+      game: { END_TURN }
+    }
+  },
+  virus_war: {
+    events: {
+      game: { SET_CELL }
+    }
+  }
+} = gamesConfig;
 
 class VirusWar extends Block {
   static template = template();
@@ -11,10 +25,15 @@ class VirusWar extends Block {
     const gameData = this.args.gameData;
     const emitter = this.args.emitter;
 
-    this.field = this.transformField(gameData.field);
+    this.socket = this.args.socket;
+    this.field = gameData.field;
     this.mapPlayersToColors = D(gameData.players).object((colors, { color, login }) => {
       colors[login] = color;
     }).$;
+    this.mapPlayersToShapes = D(gameData.players).object((shapes, { data: { shape }, login }) => {
+      shapes[login] = shape;
+    }).$;
+    this.isTopLeft = gameData.players[0].login === this.global.user.login;
 
     getAvailableCells(this.field, this.global.user).forEach((cell) => {
       console.log(cell);
@@ -22,41 +41,52 @@ class VirusWar extends Block {
       cell.isAvailable = true;
     });
 
+    emitter.on(SET_CELL, this.setCell);
+
     console.log(gameData);
-  }
-
-  transformField(field) {
-    const { login } = this.global.user;
-    const { players } = this;
-    const height = field.length - 1;
-    const width = field[0].length - 1;
-
-    if (players[0].login === login) {
-      field = D(field)
-        .reverse()
-        .map((row) => (
-          D(row).map((cell) => ({
-            ...cell,
-            y: height - cell.y
-          })).$
-        )).$;
-    } else {
-      field = D(field).map((row) => (
-        D(row)
-          .reverse()
-          .map((cell) => ({
-            ...cell,
-            x: width - cell.x
-          })).$
-      )).$;
-    }
-
-    return field;
   }
 
   emit() {
     this.socket.emit(...arguments);
   }
+
+  onClickCell(cell) {
+    if (!cell.isAvailable) {
+      return;
+    }
+
+    this.emit(SET_CELL, {
+      x: cell.x,
+      y: cell.y
+    });
+  }
+
+  endTurn = () => {
+    if (!this.args.isMyTurn) {
+      return;
+    }
+
+    this.emit(END_TURN);
+  };
+
+  setCell = (changedCell) => {
+    const {
+      field,
+      global: { user }
+    } = this;
+    const { x, y } = changedCell;
+
+    field[y][x] = changedCell;
+
+    const availableCells = getAvailableCells(field, user);
+
+    this.field = D(field).map((row) => (
+      D(row).map((cell) => ({
+        ...cell,
+        isAvailable: availableCells.includes(cell)
+      })).$
+    )).$;
+  };
 }
 
 Block.register('VirusWar', VirusWar);
