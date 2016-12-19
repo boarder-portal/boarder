acbf47caca6a113128a64afb6c580f0f5b05a3e7
droooney <d.vor7@mail.ru>
2016-12-19 21:45:08 +0500

    Rename module directory.

--- app/client/index.js
+++ app/client/index.js
@@ -1,12 +1,12 @@
 import { Block, initApp, find } from 'dwayne';
 import { injectGlobals } from './helpers';
 
-import './module/App';
-import './module/Auth';
-import './module/Home';
-import './module/NotFound';
-import './module/Games';
-import './module/Settings';
+import './modules/App';
+import './modules/Auth';
+import './modules/Home';
+import './modules/NotFound';
+import './modules/Games';
+import './modules/Settings';
 
 Block.getBlocks().forEach((block, name) => {
   Block.register(name, block.wrap(injectGlobals));
