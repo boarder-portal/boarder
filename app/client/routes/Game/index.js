dd960c9a20ab0a469fb76631f73de563d223f7a1
droooney <d.vor7@mail.ru>
2016-12-20 22:31:04 +0500

    Init game.

--- app/client/routes/Game/index.js
+++ app/client/routes/Game/index.js
@@ -1,13 +1,20 @@
 import { Block, makeRoute } from 'dwayne';
 import template from './index.pug';
 
+const games = [
+  'hexagon',
+  'set',
+  'pexeso',
+  'virus_war'
+];
+
 class Game extends Block {
   static template = template();
   static routerOptions = {
     name: 'game',
     parent: 'games',
     abstract: true,
-    path: '/:game(hexagon|pexeso|virus_war)'
+    path: `/:game(${ games.join('|') })`
   };
 }
 
