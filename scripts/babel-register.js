3f78bec2c4ae93151644d4a9e41cc4d6a983c4f7
droooney <d.vor7@mail.ru>
2018-02-21 15:33:37 +0500

    Remove dwayne and pug-runtime; fix babel.

--- scripts/babel-register.js
+++ scripts/babel-register.js
@@ -1,6 +1,7 @@
 require('babel-register')({
   babelrc: false,
   plugins: [
+    'syntax-object-rest-spread',
     'transform-es2015-modules-commonjs',
     'dynamic-import-node',
     'transform-class-properties',
