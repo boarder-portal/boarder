02f73b8d2d8c8d11a2586e9f8553b7649bbdf0e7
droooney <d.vor7@mail.ru>
2018-02-21 15:21:14 +0500

    Remove unnecessary babel transforms.

--- scripts/babel-register.js
+++ scripts/babel-register.js
@@ -3,10 +3,8 @@ require('babel-register')({
   plugins: [
     'transform-es2015-modules-commonjs',
     'dynamic-import-node',
-    'transform-object-rest-spread',
     'transform-class-properties',
     'transform-function-bind',
-    'transform-decorators-legacy',
     'transform-do-expressions',
     'transform-export-extensions'
   ]
