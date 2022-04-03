e163085b3ab4eb5fb1c004b77d7df4d0f31bb7a5
atooxa <atooxa@yandex-team.ru>
2022-04-04 01:00:07 +0500

    sw: bots count.

--- app/server/gamesData/Game/SevenWondersGame/SevenWondersGame.ts
+++ app/server/gamesData/Game/SevenWondersGame/SevenWondersGame.ts
@@ -61,7 +61,7 @@ const {
 } = GAMES_CONFIG;
 
 const ALL_CITIES = Object.values(ESevenWondersCity);
-const BOTS_COUNT = 4;
+const BOTS_COUNT = 1;
 
 class SevenWondersGame extends Game<EGame.SEVEN_WONDERS> {
   handlers = {
